b852441b2e653d92d87d405976c47a5031d91872
andybak <andy@andybak.net>
2011-06-29 08:59:30 +0100

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='django-webfaction-email',
+    version='0.5',
+    description="A Django app to provide a simple admin interface"
+                "to Webfaction's email account API.",
+    long_description=read('README.rst'),
+    author='Andy Baker',
+    author_email='andy@andybak.net',
+    license='BSD',
+    url='http://github.com/andybak/django-webfaction/',
+    packages=[
+        'django-webfaction',
+    ],
+    package_data={
+        'linkcheck': [
+            'templates/*.html',
+        ]
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
