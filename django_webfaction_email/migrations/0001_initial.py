7ad88f5750172fa5dccdc7003a54be97289f8d54
Chen Zhe <fruitschen@gmail.com>
2019-08-30 15:11:32 +0800

    add on_delete to migration

--- django_webfaction_email/migrations/0001_initial.py
+++ django_webfaction_email/migrations/0001_initial.py
@@ -29,7 +29,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('action', models.TextField(max_length=256)),
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)),
             ],
         ),
     ]
